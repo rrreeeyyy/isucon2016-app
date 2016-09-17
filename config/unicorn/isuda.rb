ddd4a661a04a0f6685194bb1d6e789b7373c96d3
Ubuntu <isucon@isucon6-qualifier.w32ey3hahsxuhi10s3xboesu4a.lx.internal.cloudapp.net>
2016-09-17 11:36:12 +0900

    update bundlers

--- config/unicorn/isuda.rb
+++ config/unicorn/isuda.rb
@@ -1,3 +1,5 @@
 worker_processes 5
 preload_app true
 timeout 120
+stderr_path File.expand_path('/tmp/log/unicorn_stderr.log', __FILE__)
+stdout_path File.expand_path('/tmp/unicorn_stdout.log', __FILE__)
