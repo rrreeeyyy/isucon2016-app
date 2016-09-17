35b22142a4ef2aaab09cf406b15c073a782960ca
Ubuntu <isucon@isucon6-qualifier.w32ey3hahsxuhi10s3xboesu4a.lx.internal.cloudapp.net>
2016-09-17 16:21:11 +0900

    Fix unicorn

--- config/unicorn/isuda.rb
+++ config/unicorn/isuda.rb
@@ -1,5 +1,8 @@
 worker_processes 5
 preload_app true
-timeout 120
-stderr_path File.expand_path('/tmp/log/unicorn_stderr.log', __FILE__)
-stdout_path File.expand_path('/tmp/unicorn_stdout.log', __FILE__)
+timeout 12000000
+#stderr_path File.expand_path('/tmp/log/unicorn_stderr.log', __FILE__)
+#stdout_path File.expand_path('/tmp/unicorn_stdout.log', __FILE__)
+log = '/tmp/unicorn.log'
+stderr_path '/tmp/unicorn.log'
+stdout_path '/tmp/unicorn.log'
